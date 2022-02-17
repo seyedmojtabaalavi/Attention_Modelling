a39fb360f0e494e114821cadc7dfd5469bbe0992
seyedmojtabaalavi <alavimojtabaseyed@gmail.com>
2022-02-17 07:31:33 +0100

    Update Networking.py

--- Networking.py
+++ Networking.py
@@ -7,7 +7,8 @@ class network:
     def __init__(self):
         self.connection_list = []
         self.neuron_lists = []
-        self.weight_list = []  ##################
+        self.weight_list = []
+        self.id = 0
 
     # ------------------------------------------------------------------------------------------------------------------
     # Connection
@@ -17,12 +18,10 @@ class network:
         flag = False
         if length_list != 0:
             for i in range(length_list):
-                if self.connection_list[i][0].get_id() == presynapse.get_id() and \
-                                self.connection_list[i][0].get_type() == presynapse.get_type():
+                if self.connection_list[i][0].get_id() == presynapse.get_id():
                     flag1 = True
                     for j in range(1, len(self.connection_list[i])):
-                        if self.connection_list[i][j].get_id() == postsynapse.get_id and \
-                                        self.connection_list[i][j].get_type() == postsynapse.get_type():
+                        if self.connection_list[i][j].get_id() == postsynapse.get_id():
                             flag1 = False
                             break
 
@@ -44,6 +43,8 @@ class network:
         if number == 1:
             if neuron_type == 'FEF':
                 FEF = Neurons.FEF()
+                FEF.set_id(self.id)
+                self.id += 1
                 self.neuron_lists.append(FEF)
                 return FEF
         elif number > 1:
@@ -51,6 +52,8 @@ class network:
             if neuron_type == 'FEF':
                 for i in range(number):
                     FEF = Neurons.FEF()
+                    FEF.set_id(self.id)
+                    self.id += 1
                     self.neuron_lists.append(FEF)
                     neurons_list.append(FEF)
                     del FEF
